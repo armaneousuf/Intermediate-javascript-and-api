60de6111818caac0a1745d19047d37ebc05b9e38
armaneousuf <armaneousuf97@gmail.com>
2026-02-03 22:29:39 +0600

    refactor: changed traditional function to arrow function

--- 29-BASIC-ES6/tasks/task-6.js
+++ 29-BASIC-ES6/tasks/task-6.js
@@ -1,20 +1,19 @@
-function getBiggestNumberInArrays(numbers1, numbers2) {
-    let biggestNumber = -Infinity; 
+const getBiggestNumberInArrays = (number1, number2) => {
+    let biggestNumber = -Infinity;
 
-    for (const number of numbers1) {
+    for (const number of number1) {
         if (number > biggestNumber) {
             biggestNumber = number;
         }
     }
 
-    for (const number of numbers2) {
+    for (const number of number2) {
         if (number > biggestNumber) {
             biggestNumber = number;
         }
     }
 
-    return biggestNumber;
-
+    return biggestNumber
 }
 
 console.log(getBiggestNumberInArrays([7, 2, 9], [15, 28, 42]));
