55e232edb6cc91cda1aef203ed9ec1d8ad52b09b
armaneousuf <armaneousuf97@gmail.com>
2026-02-03 14:42:02 +0600

    feat: added multiply

--- 29-BASIC-ES6/playground/default.js
+++ 29-BASIC-ES6/playground/default.js
@@ -4,3 +4,11 @@ function sum(num1 = 10, num2 = 20) {
 
 const total = sum(10);
 console.log(total);
+
+
+function multiply(num1, num2 = 1) {
+    const total = num1 * num2;
+    return total;
+}
+
+console.log(multiply(2));
