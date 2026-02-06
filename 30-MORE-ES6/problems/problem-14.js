adf233954bb856e7d842c2b3507d55313b422a20
armaneousuf <armaneousuf97@gmail.com>
2026-02-06 18:55:47 +0600

    feat: solved problem-14

---
+++ 30-MORE-ES6/problems/problem-14.js
@@ -0,0 +1,14 @@
+// You have an array of numbers. Use the appropriate method to print each number multiplied by its index position.
+
+// Here's the array:
+
+const numbers = [5, 10, 15, 20, 25];
+
+// For example:
+// - 5 is at index 0, so print 5 × 0 = 0
+// - 10 is at index 1, so print 10 × 1 = 10
+// - 15 is at index 2, so print 15 × 2 = 30
+// - etc.
+
+const result = numbers.map((x, y) => x * y);
+console.log(result); 
