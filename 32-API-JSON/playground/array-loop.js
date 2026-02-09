98b2a0f700d8df01ac866f4fc0222a8c2223e518
armaneousuf <armaneousuf97@gmail.com>
2026-02-09 14:01:54 +0600

    feat: revisiting the previous problems

---
+++ 32-API-JSON/playground/array-loop.js
@@ -0,0 +1,7 @@
+const numbers = [1, 5, 2, 7, 4];
+
+console.log('length:', numbers.length);
+for (let i = 0; i < numbers.length; i++){
+    console.log(numbers[i]);
+    console.log('i =', i, 'value =', numbers[i]);
+}
