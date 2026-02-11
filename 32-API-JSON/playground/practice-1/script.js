7034d56a0cf088fd9c847f8c97e5c44477d4a2de
armaneousuf <armaneousuf97@gmail.com>
2026-02-11 16:35:45 +0600

    feat: practicing api with demo data

---
+++ 32-API-JSON/playground/practice-1/script.js
@@ -0,0 +1,18 @@
+const loadPost = () => {
+  const url = "https://jsonplaceholder.typicode.com/posts";
+    fetch(url)
+        .then((res) => res.json())
+        .then((json) => displayPost(json))
+        .catch(error => console.log('Error:', error));
+};
+
+const displayPost = (posts) => {
+    const container = document.querySelector('#post-container');
+    container.innerText = '';
+
+  posts.forEach((post) => {
+      const p = document.createElement('p');
+      p.innerText = post.title;
+      container.appendChild(p);
+  });
+};
