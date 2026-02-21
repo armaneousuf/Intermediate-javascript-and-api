211c19c73ff7edc6c1ce8109061a500d7e56f93b
armaneousuf <armaneousuf97@gmail.com>
2026-02-21 20:10:15 +0600

    style: format code

--- 34-MORE-JS/problems/problem-09.js
+++ 34-MORE-JS/problems/problem-09.js
@@ -1,11 +1,11 @@
 function showCurrentTime() {
-    const timerId = setInterval(() => {
-        const currentTime = new Date().toLocaleTimeString();
-        console.log(currentTime);
-    }, 1000);
+  const timerId = setInterval(() => {
+    const currentTime = new Date().toLocaleTimeString();
+    console.log(currentTime);
+  }, 1000);
 
-    setTimeout(() => {
-        clearInterval(timerId);
-    }, 10000);
+  setTimeout(() => {
+    clearInterval(timerId);
+  }, 10000);
 }
-showCurrentTime()
+showCurrentTime();
